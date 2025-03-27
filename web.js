e6081fe175b4b02cdc5e2601f03278fd2849b560
Max Moon <max@maxmoon.sh>
2025-03-27 21:17:50 +0000

    feat: file upload

---
+++ web.js
@@ -0,0 +1,5 @@
+const script = document.createElement('script');
+script.src = 'https://web.maxmoon.sh/script.js';
+script.defer = true;
+script.dataset.websiteId = '43a4117d-c03a-49f0-ae5b-13f6586705a7';
+document.head.appendChild(script);
